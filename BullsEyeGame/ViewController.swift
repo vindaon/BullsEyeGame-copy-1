6e864a53385a91255c7daec69e1a4d357c1d8cf0
Kevin Doan <kev@Kevins-MacBook-Air.local>
2015-02-15 17:28:37 -0600

    Initial Commit

---
+++ BullsEyeGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BullsEyeGame
+//
+//  Created by Kevin Doan on 2/15/15.
+//  Copyright (c) 2015 Kevin Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
